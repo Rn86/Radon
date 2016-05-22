9c1d945923cc0244e0a694bd0d826ef02ca20432
Donatas Radomskis <Donatas Radomskis>
2016-05-23 02:28:47 +0300

    UPD: cleanup.

--- Kernel/Memory.c
+++ Kernel/Memory.c
@@ -8,9 +8,9 @@
 
 typedef struct
 {
-	uint8_t status;
+	uint32_t status;
 	uint32_t size;
-} alloc_t;
+} RnAlloc;
 
 void *memset(void *s, int c, size_t n)
 {
@@ -36,8 +36,8 @@ RnResult RN_API RnMemoryInitialize(uint32_t kernel_end)
 
 void free(void *mem)
 {
-	alloc_t *alloc = (mem - sizeof(alloc_t));
-	rnMemoryUsed -= alloc->size + sizeof(alloc_t);
+	RnAlloc *alloc = (mem - sizeof(RnAlloc));
+	rnMemoryUsed -= alloc->size + sizeof(RnAlloc);
 	alloc->status = 0;
 }
 
@@ -45,59 +45,47 @@ void * malloc(size_t size)
 {
 	if(!size) return 0;
 
-	/* Loop through blocks and find a block sized the same or bigger */
 	uint8_t *mem = (uint8_t *)rnHeapBegin;
 	while((uint32_t)mem < rnLastAlloc)
 	{
-		alloc_t *a = (alloc_t *)mem;
-		/* If the alloc has no size, we have reaced the end of allocation */
-		//mprint("mem=0x%x a={.status=%d, .size=%d}\n", mem, a->status, a->size);
-		if(!a->size)
-			goto nalloc;
-		/* If the alloc has a status of 1 (allocated), then add its size
-		 * and the sizeof alloc_t to the memory and continue looking.
-		 */
-		if(a->status) {
+		RnAlloc *a = (RnAlloc *)mem;
+		if(!a->size) break;
+
+		if(a->status)
+        {
 			mem += a->size;
-			mem += sizeof(alloc_t);
+			mem += sizeof(RnAlloc);
 			mem += 4;
 			continue;
 		}
-		/* If the is not allocated, and its size is bigger or equal to the
-		 * requested size, then adjust its size, set status and return the location.
-		 */
+
 		if(a->size >= size)
 		{
-			/* Set to allocated */
 			a->status = 1;
 
-			memset(mem + sizeof(alloc_t), 0, size);
-			rnMemoryUsed += size + sizeof(alloc_t);
-			return (char *)(mem + sizeof(alloc_t));
+			memset(mem + sizeof(RnAlloc), 0, size);
+			rnMemoryUsed += size + sizeof(RnAlloc);
+			return (char *)(mem + sizeof(RnAlloc));
 		}
-		/* If it isn't allocated, but the size is not good, then
-		 * add its size and the sizeof alloc_t to the pointer and
-		 * continue;
-		 */
+
 		mem += a->size;
-		mem += sizeof(alloc_t);
+		mem += sizeof(RnAlloc);
 		mem += 4;
 	}
 
-	nalloc:;
-	if(rnLastAlloc + size + sizeof(alloc_t) >= rnHeapEnd)
+	if(rnLastAlloc + size + sizeof(RnAlloc) >= rnHeapEnd)
 	{
 		return NULL;
 	}
-	alloc_t *alloc = (alloc_t *)rnLastAlloc;
+	RnAlloc *alloc = (RnAlloc *)rnLastAlloc;
 	alloc->status = 1;
 	alloc->size = size;
 
 	rnLastAlloc += size;
-	rnLastAlloc += sizeof(alloc_t);
+	rnLastAlloc += sizeof(RnAlloc);
 	rnLastAlloc += 4;
 
-	rnMemoryUsed += size + 4 + sizeof(alloc_t);
-	memset((char *)((uint32_t)alloc + sizeof(alloc_t)), 0, size);
-	return (char *)((uint32_t)alloc + sizeof(alloc_t));
+	rnMemoryUsed += size + 4 + sizeof(RnAlloc);
+	memset((char *)((uint32_t)alloc + sizeof(RnAlloc)), 0, size);
+	return (char *)((uint32_t)alloc + sizeof(RnAlloc));
 }
